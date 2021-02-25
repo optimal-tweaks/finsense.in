63fe89d176e267faffbf457148ea28aceee1c04e
shubham13666 <shubham13666@gmail.com>
2021-02-25 22:41:27 +0530

    routing fixes

--- public/js/case_study.js
+++ public/js/case_study.js
@@ -16,7 +16,7 @@ function getdata() {
     let loopIndex = 0;
     for (let [key, value] of Object.entries(data)) {
 
-      posts_div.innerHTML = "<a " + `${loopIndex == 0 ? "id='w-node-470d3160ab99-3d151751'" : " "}` + "href='https://finsense.in/case_study/" + key + "'" +
+      posts_div.innerHTML = "<a " + `${loopIndex == 0 ? "id='w-node-470d3160ab99-3d151751'" : " "}` + "href='https://us-central1-finsense-wealth-minimal.cloudfunctions.net/app/case_study/" + key + "'" +
         "class='div-block-33 w-inline-block'>" +
         "<div class='div-block-34' style='background-image:url(" + value.imageURL + ")'></div>" +
         "<div class='div-block-35'>" +
